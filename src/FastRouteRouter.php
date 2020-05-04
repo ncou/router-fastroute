28f6f12241c5dbe74d2f7f7220da84f872329d10
ncou <nicolas_courade@yahoo.fr>
2020-05-04 23:38:27 +0200

    update

--- src/FastRouteRouter.php
+++ src/FastRouteRouter.php
@@ -8,11 +8,11 @@ use Chiron\Router\Traits\MiddlewareAwareInterface;
 use Chiron\Router\Traits\MiddlewareAwareTrait;
 use Chiron\Router\Traits\RouteCollectionInterface;
 use Chiron\Router\Traits\RouteCollectionTrait;
+use Chiron\Router\Exception\RouterException;
 use Chiron\Pipe\PipelineBuilder;
 use Chiron\Router\RouterInterface;
 use Chiron\Router\Route;
 use Chiron\Router\Method;
-use Chiron\Router\RequestHandler;
 use Chiron\Router\RouteCollectorInterface;
 use Chiron\Router\RouteGroup;
 use Chiron\Router\MatchingResult;
@@ -29,7 +29,7 @@ use RuntimeException;
 //https://github.com/zendframework/zend-expressive-fastroute/blob/master/src/FastRouteRouter.php
 //https://github.com/Wandu/Router/blob/master/RouteCollection.php
 
-// TODO : il manque head et options dans la phpdoc
+// TODO : il manque head et options dans la phpdoc, et le check sur la collision n'est plus d'actualité !!!!
 /**
  * Aggregate routes for the router.
  *
@@ -50,7 +50,7 @@ use RuntimeException;
  * attaching via one of the exposed methods, and will raise an exception when a
  * collision occurs.
  */
-class FastRouteRouter implements RouterInterface
+final class FastRouteRouter implements RouterInterface
 {
     /** @var FastRoute\RouteParser */
     private $parser;
@@ -64,11 +64,17 @@ class FastRouteRouter implements RouterInterface
     private $routes = [];
 
     /**
+     * @var bool
+     */
+    private $isInjected = false;
+
+    /**
      * @var array
      */
     // TODO : regarder ici : https://github.com/ncou/router-group-middleware/blob/master/src/Router/Router.php#L25
     // TODO : regarder ici : https://github.com/ncou/php-router-group-middleware/blob/master/src/Router.php#L26
     // TODO : faire un tableau plus simple et ensuite dans le constructeur faire un array walk pour ajouter ces patterns.
+    // TODO : on devrait pas utiliser plutot cet regex pour les UUID : '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}'
     private $patternMatchers = [
         '/{(.+?):number}/'        => '{$1:[0-9]+}',
         '/{(.+?):word}/'          => '{$1:[a-zA-Z]+}',
@@ -76,6 +82,8 @@ class FastRouteRouter implements RouterInterface
         '/{(.+?):slug}/'          => '{$1:[a-z0-9-]+}',
         '/{(.+?):uuid}/'          => '{$1:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}',
     ];
+
+
 
     /*
 
@@ -92,7 +100,7 @@ class FastRouteRouter implements RouterInterface
     /*
     //https://github.com/codeigniter4/CodeIgniter4/blob/develop/system/Router/RouteCollection.php#L122
 
-    protected $placeholders = [
+    private $placeholders = [
         'any'      => '.*',
         'segment'  => '[^/]+',
         'alphanum' => '[a-zA-Z0-9]+',
@@ -104,10 +112,6 @@ class FastRouteRouter implements RouterInterface
 
     */
 
-    /**
-     * @var string Can be used to ignore leading part of the Request URL (if main file lives in subdirectory of host)
-     */
-    private $basePath;
 
     /**
      * Constructor.
@@ -117,7 +121,7 @@ class FastRouteRouter implements RouterInterface
      */
     // TODO : créer un constructeur qui prendra en paramétre un routeCollector, ca évitera de faire un appel à setRouteCollector() !!!!
     // TODO : virer le DataGenerator qui est en paramétre et faire un new directement dans le constructeur.
-    public function __construct(string $basePath)
+    public function __construct()
     {
         $this->parser = new RouteParser();
         // build parent route collector
@@ -128,8 +132,6 @@ class FastRouteRouter implements RouterInterface
         array_walk($this->patternMatchers, function ($value, $key) {
             $this->addPatternMatcher($key, $value);
         });*/
-
-        $this->basePath = '/' . ltrim($basePath, '/'); //TODO : utiliser plutot ce bout de code         sprintf('/%s', ltrim($path, '/'));
     }
 
     /**
@@ -151,36 +153,16 @@ class FastRouteRouter implements RouterInterface
     }
 
     /**
-     * Set the base path.
-     * Useful if you are running your application from a subdirectory.
-     */
-/*
-    public function setBasePath(string $basePath): void
-    {
-        $this->basePath = rtrim($basePath, '/');
-        //$this->basePath = $basePath;
-        //$this->basePath = '/' . ltrim($basePath, '/');
-    }*/
-
-    /**
-     * Get the router base path.
-     * Useful if you are running your application from a subdirectory.
-     */
-/*
-    public function getBasePath(): string
-    {
-        return $this->basePath;
-    }*/
-
-    /**
-     * Add a route to the collection.
+     * Add a Route to the collection, and return the route for chaining calls.
      *
      * @param Route $route
      */
-    // TODO : il faudrait que le addRoute retourne la route ajoutée, cela permettra de chainer les commandes "ex : $router->addRoute($route1)->name('home')"
-    public function addRoute(Route $route): void
+    public function addRoute(Route $route): Route
     {
+        // TODO : Lever une logica exception si on essaye d'ajouter une route, alors que que le booleen $this->isInjected est à true. Car cela n'a pas de sens d'ajouter une route aprés avoir appellé la méthode ->match de cette classe !!!!
         $this->routes[] = $route;
+
+        return $route;
     }
 
     /**
@@ -225,6 +207,7 @@ class FastRouteRouter implements RouterInterface
      *
      * @return string
      */
+    // TODO : méthode à virer elle ne sert à rien car on a viré la notion de "basePath" dans le router.
     public function urlFor(string $routeName, array $substitutions = [], array $queryParams = []): string
     {
         $url = $this->relativeUrlFor($routeName, $substitutions, $queryParams);
@@ -257,6 +240,7 @@ class FastRouteRouter implements RouterInterface
         return FastRouteUrlGenerator::generate($route->getPath(), $substitutions, $queryParams);
     }
 
+
     public function match(ServerRequestInterface $request): MatchingResult
     {
         // prepare routes
@@ -266,9 +250,9 @@ class FastRouteRouter implements RouterInterface
         $dispatcher = $this->getDispatcher();
 
         $httpMethod = $request->getMethod();
-        $uri = rawurldecode($request->getUri()->getPath()); //$uri = '/' . ltrim($request->getUri()->getPath(), '/');
+        $uri = $request->getUri()->getPath(); //$uri = '/' . ltrim($request->getUri()->getPath(), '/');
 
-        $result = $dispatcher->dispatch($httpMethod, $uri);
+        $result = $dispatcher->dispatch($httpMethod, rawurldecode($uri));
 
         //die(var_dump($result));
 
@@ -294,7 +278,7 @@ class FastRouteRouter implements RouterInterface
             return MatchingResult::fromRouteFailure($result[1]);
         }
 
-        return MatchingResult::fromRouteFailure(MatchingResult::HTTP_METHOD_ANY);
+        return MatchingResult::fromRouteFailure(Method::ANY);
     }
 
     /**
@@ -314,8 +298,14 @@ class FastRouteRouter implements RouterInterface
      *
      * @param \Psr\Http\Message\ServerRequestInterface $request
      */
+    // TODO : passer uniquement en paramétre un UriInterface plutot que toute la Request !!!!
     private function injectRoutes(ServerRequestInterface $request): void
     {
+        // only inject routes once.
+        if ($this->isInjected) {
+            return;
+        }
+
         foreach ($this->routes as $route) {
             // check for scheme condition
             if (! is_null($route->getScheme()) && $route->getScheme() !== $request->getUri()->getScheme()) {
@@ -334,8 +324,10 @@ class FastRouteRouter implements RouterInterface
             $routePath = $this->replaceWordPatterns($routePath);
 
             //Each added route must inherit basePath prefix
-            $this->injectRoute($route, $route->getAllowedMethods(), $this->basePath . $routePath);
+            $this->injectRoute($route, $route->getAllowedMethods(), $routePath);
         }
+
+        $this->isInjected = true;
     }
 
     /**
@@ -379,17 +371,19 @@ class FastRouteRouter implements RouterInterface
      * @param string   $routeId
      * @param string[] $httpMethod
      * @param string   $routePath
+     *
+     * @throws RouterException If two routes match the same url+method, or if the route is shadowed by a previous route.
      */
     private function injectRoute(Route $route, array $httpMethod, string $routePath): void
     {
         $routeDatas = $this->parser->parse($routePath);
         foreach ($httpMethod as $method) {
             foreach ($routeDatas as $routeData) {
-                // TODO : réactiver le try catch si on souhaite pouvoir gérer les doublons de routes.
-                //try {
-                $this->generator->addRoute($method, $routeData, $route);
-                //} catch (\Throwable $e) {
-                //}
+                try {
+                    $this->generator->addRoute($method, $routeData, $route);
+                } catch (\FastRoute\BadRouteException $e) {
+                    throw new RouterException($e->getMessage());
+                }
             }
         }
     }
---
+++ src/Provider/FastRouteRouterServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Chiron (http://www.chironframework.com).
+ *
+ * @see      https://github.com/ncou/Chiron
+ *
+ * @license   https://github.com/ncou/Chiron/blob/master/licenses/LICENSE.md (MIT License)
+ */
+
+//https://github.com/userfrosting/UserFrosting/blob/master/app/system/ServicesProvider.php
+//https://github.com/slimphp/Slim/blob/3.x/Slim/DefaultServicesProvider.php
+declare(strict_types=1);
+
+namespace Chiron\Router\FastRoute\Provider;
+
+//use Chiron\Http\Middleware\ErrorHandlerMiddleware;
+use Chiron\Container\Container;
+use Chiron\Container\InvokerInterface;
+use Chiron\Bootload\ServiceProvider\ServiceProviderInterface;
+use Chiron\Kernel;
+use Chiron\Router\FastRoute\FastRouteRouter;
+use Chiron\Router\RouteCollector;
+use Chiron\Router\RouterInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Chiron\Config\ConfigManager;
+use Chiron\Container\BindingInterface;
+
+/**
+ * Chiron system services provider.
+ *
+ * Registers system services for Chiron, such as config manager, middleware router and dispatcher...
+ */
+class FastRouteRouterServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * Register Chiron system services.
+     *
+     * @param ContainerInterface $container A DI container implementing ArrayAccess and container-interop.
+     */
+    public function register(BindingInterface $container): void
+    {
+        // register router object
+        $container->share(RouterInterface::class, FastRouteRouter::class);
+    }
+}
